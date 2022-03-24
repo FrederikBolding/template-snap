1aa0e25310f20b00b9cb18eb12e4702f8654e399
Frederik Bolding <frederik.bolding@gmail.com>
2022-03-24 14:16:52 +0100

    Encryption test

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
+const ethUtil = require('ethereumjs-util');
+const sigUtil = require('@metamask/eth-sig-util');
+
+async function encrypt(data) {
+  const accounts = await wallet.request({
+    method: 'eth_accounts',
+    params: [],
+  });
+  console.log(accounts);
+  const publicKey = await wallet.request({
+    method: 'eth_getEncryptionPublicKey',
+    params: [accounts[0]], // you must have access to the specified account
+  });
+  console.log(publicKey);
+  const encryptedMessage = ethUtil.bufferToHex(
+    Buffer.from(
+      JSON.stringify(
+        sigUtil.encrypt({
+          publicKey,
+          data,
+          version: 'x25519-xsalsa20-poly1305',
+        }),
+      ),
+      'utf8',
+    ),
+  );
+  console.log(encryptedMessage);
+  return encryptedMessage;
+}
+
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
   switch (requestObject.method) {
     case 'hello':
-      return wallet.request({
-        method: 'snap_confirm',
-        params: [
-          {
-            prompt: `Hello, ${originString}!`,
-            description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
-          },
-        ],
-      });
+      return await encrypt('hello world!');
     default:
       throw new Error('Method not found.');
   }
